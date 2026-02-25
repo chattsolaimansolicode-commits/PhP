e377e44e78a03e646e40655a02b9b73327915124
chattsolaimansolicode-commits <chattsolaiman.solicode@gmail.com>
2026-02-25 10:29:54 +0000

    Enhance PHP calculator form and functionality

--- prototype/proto.php
+++ prototype/proto.php
@@ -1,43 +1,53 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>PHP Calculator</title>
 </head>
-
 <body>
 
-    <h2>Calculator</h2>
+<h2>Simple Calculator</h2>
 
-    <form method="POST">
-        <input type="number" name="num1">
+<form method="POST">
+    <input type="number" name="num1" required>
 
-        <select name="op">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
+    <select name="operation">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
 
-        <input type="number" name="num2">
-        <br><br>
+    <input type="number" name="num2" required>
+    <br><br>
 
-        <input type="submit" value="Calculate">
-    </form>
+    <input type="submit" value="Calculate">
+</form>
 
-    <?php
-    
-   for ($i = 1 ; $i <= 20 ;$i++){
-    echo $i ."<br>";
+<?php
+$result = "";
 
-   }
-   $i = 1;
-   for($i ){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num1 = $_POST["num1"];
+    $num2 = $_POST["num2"];
+    $op   = $_POST["operation"];
 
-   }
-    ?>
-    
+    if ($op == "+") {
+        $result = $num1 + $num2;
+    } elseif ($op == "-") {
+        $result = $num1 - $num2;
+    } elseif ($op == "*") {
+        $result = $num1 * $num2;
+    } elseif ($op == "/") {
+        if ($num2 == 0) {
+            $result = "Error: division by zero";
+        } else {
+            $result = $num1 / $num2;
+        }
+    }
+}
+?>
+
+<h3>Result: <?php echo $result; ?></h3>
 
 </body>
-
 </html>
